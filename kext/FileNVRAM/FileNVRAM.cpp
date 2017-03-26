meklort/FileNVRAM

commit 6dd710025e867d60de0cbc93bfe93a2eba35c746

Fixed buffer lenght including null char
Fixed buffer lenght including null char calulating the size of
GUID+separator+key + null char using snprintf

kext/FileNVRAM/FileNVRAM.cpp
@@ -217,7 +217,7 @@ void FileNVRAM::copyUnserialzedData(const char* prefix, OSDictionary* dict)
             
             if(prefix)
             {
-                size_t size = strlen(prefix) + sizeof(NVRAM_SEPERATOR) + strlen(key->getCStringNoCopy());
+                size_t size = strlen(prefix) + sizeof(NVRAM_SEPERATOR) + strlen(key->getCStringNoCopy()) +1;
                 char* newKey = (char*)IOMalloc(size);
                 
                 snprintf(newKey, size, "%s%s%s", prefix, NVRAM_SEPERATOR, key->getCStringNoCopy());
@@ -266,7 +266,7 @@ void FileNVRAM::copyEntryProperties(const char* prefix, IORegistryEntry* entry)
                 
                 if(prefix)
                 {
-                    size_t size = strlen(prefix) + sizeof(NVRAM_SEPERATOR) + strlen(name);
+                    size_t size = strlen(prefix) + sizeof(NVRAM_SEPERATOR) + strlen(name) +1;
                     char* newPrefix = (char*)IOMalloc(size);
                     
                     snprintf(newPrefix, size, "%s%s%s", prefix, NVRAM_SEPERATOR, name);
@@ -305,7 +305,7 @@ void FileNVRAM::copyEntryProperties(const char* prefix, IORegistryEntry* entry)
             
             if(prefix)
             {
-                size_t size = strlen(prefix) + sizeof(NVRAM_SEPERATOR) + strlen(key->getCStringNoCopy());
+                size_t size = strlen(prefix) + sizeof(NVRAM_SEPERATOR) + strlen(key->getCStringNoCopy()) +1;
                 char* newKey = (char*)IOMalloc(size);
                 
                 snprintf(newKey, size, "%s%s%s", prefix, NVRAM_SEPERATOR, key->getCStringNoCopy());

commit 6160f7daa1f4ccf3f67726b14da307be78df50ac

Buffer length fixed
Fixed a bug (introduced in commit 6dd7100) calculating the plist size
during write operation.

kext/FileNVRAM/FileNVRAM.cpp
@@ -449,7 +449,7 @@ void FileNVRAM::doSync(void)
     s->addString(NVRAM_FILE_FOOTER);
     
     
-    int error =	write_buffer(s->text());
+    int error =	write_buffer(s->text(), s->getLength());
     if(error)
     {
         LOG(ERROR, "Unable to write to %s, errno %d\n", mFilePath->getCStringNoCopy(), error);
@@ -900,20 +900,17 @@ OSObject* FileNVRAM::cast(const OSSymbol* key, OSObject* obj)
     return obj;
 }
 
-IOReturn FileNVRAM::write_buffer(char* buffer)
+IOReturn FileNVRAM::write_buffer(char* buffer, int length)
 {
     IOReturn error = 0;
     
     if(mReadOnly) return error;
-    
-    int length = (int)strlen(buffer);
-    int ares;
+
     struct vnode * vp;
     
     if(mCtx)
     {
-        // O_WRONLY
-        if((error = vnode_open(FILE_NVRAM_PATH, (O_WRONLY | O_CREAT | O_TRUNC | FWRITE | O_NOFOLLOW), S_IRUSR | S_IWUSR, VNODE_LOOKUP_NOFOLLOW, &vp, mCtx)))
+        if((error = vnode_open(FILE_NVRAM_PATH, (O_TRUNC | O_CREAT | FWRITE | O_NOFOLLOW), S_IRUSR | S_IWUSR, VNODE_LOOKUP_NOFOLLOW, &vp, mCtx)))
         {
             LOG(ERROR, "error, vnode_open(%s) failed with error %d!\n", FILE_NVRAM_PATH, error);
             
@@ -923,8 +920,7 @@ IOReturn FileNVRAM::write_buffer(char* buffer)
         {
             if((error = vnode_isreg(vp)) == VREG)
             {
-                // 10.6 and later
-                if((error = vn_rdwr(UIO_WRITE, vp, buffer, length, 0, UIO_SYSSPACE, IO_NOCACHE|IO_NODELOCKED|IO_UNIT, vfs_context_ucred(mCtx), &ares/*(int *) 0*/, vfs_context_proc(mCtx))))
+                if((error = vn_rdwr(UIO_WRITE, vp, (caddr_t)buffer, length, (off_t)0, UIO_SYSSPACE, IO_NOCACHE|IO_NODELOCKED|IO_UNIT, vfs_context_ucred(mCtx), (int *) 0, vfs_context_proc(mCtx))))
                 {
                     LOG(ERROR, "error, vn_rdwr(%s) failed with error %d!\n", FILE_NVRAM_PATH, error);
                 }

kext/FileNVRAM/FileNVRAM.h
@@ -167,7 +167,7 @@ private:
     
     static IOReturn dispatchCommand( OSObject* owner, void* arg0, void* arg1, void* arg2, void* arg3 );
     
-    virtual IOReturn write_buffer(char* buffer);
+    virtual IOReturn write_buffer(char* buffer, int length);
     virtual IOReturn read_buffer(char** buffer, uint64_t* length);
     
     bool mReadOnly;